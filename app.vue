1dd3528412ea24f789f64af38875e8dee990ec9c
Eduardo López <loama18@gmail.com>
2024-08-02 23:43:15 +0200

    add link to results

--- app.vue
+++ app.vue
@@ -3,6 +3,13 @@
   <div
     class="title fixed top-0 left-0 w-full h-12 text-center leading-[50px] bg-white text-black border-b border-zinc-300 z-20">
     Nobel Track
+
+    <NuxtLink
+      to="/results"
+      class="absolute right-2 top-0 border border-zinc-200 leading-8 top-2 text-sm h-8 rounded-2xl px-2 bg-black text-white"
+      v-if="$route.path !== '/results'"
+      >See Results
+    </NuxtLink>
   </div>
   <div class="pt-12 bg-[#F3F1EF] min-h-screen">
     <NuxtPage />
